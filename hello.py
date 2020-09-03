24752714ea271f5da4205add0cb1adf69d4027f8
Carlos Simon <casimon@redhat.com>
2020-09-02 20:56:54 -0500

    send client request

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
+import requests
+import os
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/send')
+def send():
+    try:
+        return "response: {}".format(requests.get(os.environ.get('SERVER_URL')).json())
+    except Exception as ex:
+        print(ex)
+        return "error"
 
 if __name__ == '__main__':
     app.run()
